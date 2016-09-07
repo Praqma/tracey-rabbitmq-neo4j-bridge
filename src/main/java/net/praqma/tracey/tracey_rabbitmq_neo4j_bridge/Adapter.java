64e5a299a7a9a1afc1a7a593c9a730bbb0fc83df
Sofus Albertsen <soa@praqma.net>
2016-09-07 10:07:08 +0200

    added jitpack repo, modified Adapter to take input files for properties, and changed default AMQP properties

--- src/main/java/net/praqma/tracey/tracey_rabbitmq_neo4j_bridge/Adapter.java
+++ src/main/java/net/praqma/tracey/tracey_rabbitmq_neo4j_bridge/Adapter.java
@@ -17,16 +17,26 @@ import java.util.Properties;
 public class Adapter {
 
     public static void main(String[] args) throws IOException {
-	ClassLoader classLoader = Adapter.class.getClassLoader();
-    	
-        File amqpFile = new File(classLoader.getResource("AMQP.properties").getFile());
-        File neoFile = new File(classLoader.getResource("NEO4J.properties").getFile());
-        Properties amqpprop = new Properties();
-        amqpprop.load(new FileReader(amqpFile));
-        Properties neoprop = new Properties();
-        neoprop.load(new FileReader(neoFile));
-        AMQPFacade amqp = new AMQPFacade(amqpprop);
-        Tracey2Neo neo= new Tracey2Neo(neoprop);
-        amqp.recieveEvents(neo);
-    }
+    	ClassLoader classLoader = Adapter.class.getClassLoader();
+		// If no files are specified in the args, take the build in props.
+		File amqpFile = new File(classLoader.getResource("AMQP.properties").getFile());
+		File neoFile = new File(classLoader.getResource("NEO4J.properties").getFile());
+
+		Properties amqpprop = new Properties();
+		Properties neoprop = new Properties();
+
+		if (args.length == 2) {
+			amqpFile = new File(args[0]);
+			neoFile = new File(args[1]);
+
+		} else {
+			System.out.println("Need to parse in two arguments: 1. AMQP properties destination, 2. NEO4J properties. \n using the build in defaults");
+		}
+
+		amqpprop.load(new FileReader(amqpFile));
+		neoprop.load(new FileReader(neoFile));
+		AMQPFacade amqp = new AMQPFacade(amqpprop);
+		Tracey2Neo neo = new Tracey2Neo(neoprop);
+		amqp.recieveEvents(neo);
+	}
 }
